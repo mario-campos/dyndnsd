12289611bbefc704292f43e5f3983c7c6b720e8d
Mario Campos <mario@mariocampos.io>
2017-06-21 08:31:37 -0500

    add doc comments to header files

--- src/ast.h
+++ src/ast.h
@@ -20,7 +20,22 @@ struct ast {
     const char		*url;
 };
 
+/*
+ * Allocate a new Abstract Syntax Tree (AST).
+ */
 struct ast *new_ast(struct ast_iface *, const char *);
+
+/*
+ * Allocate a new AST node for interfaces.
+ */
 struct ast_iface *new_ast_iface(const char *, struct ast_domain *, const char *);
+
+/*
+ * Allocate a new AST node for domain names.
+ */
 struct ast_domain *new_ast_domain(const char *, const char *);
+
+/*
+ * Check the AST for illogical configurations.
+ */
 bool valid_ast(struct ast *);
--- src/param.h
+++ src/param.h
@@ -6,7 +6,23 @@ struct param {
     char	 *value;
 };
 
+/*
+ * Parse the HTTP query parameters.
+ */
 struct param *getparams(const char *);
+
+/*
+ * Define a query parameter, new or existing.
+ */
 struct param *setparam(struct param *, const char *, const char *);
+
+/*
+ * Construct a new URL from an old URL and the new
+ * query parameters.
+ */
 char *mkurl(const char *, struct param *);
+
+/*
+ * Deallocate query parameters.
+ */
 void freeparams(struct param *);
