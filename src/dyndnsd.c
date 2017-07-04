68689fe3e099a754614c59242188de6be5e67e0d
Mario Campos <mario@mariocampos.io>
2017-07-03 20:57:43 -0500

    fixes issue #2: now using route(4) event handling

--- src/dyndnsd.c
+++ src/dyndnsd.c
@@ -1,6 +1,12 @@
+#include <sys/queue.h>
+#include <sys/socket.h>
+
 #include <arpa/inet.h>
+#include <net/if.h>
+#include <net/route.h>
 #include <netinet/in.h>
 
+#include <assert.h>
 #include <err.h>
 #include <ifaddrs.h>
 #include <stdint.h>
@@ -15,17 +21,19 @@
 #include "ast.h"
 #include "config.h"
 #include "pathnames.h"
-#include "route.h"
 
 extern char    *__progname;
 extern FILE    *yyin;
 extern int 	yyparse();
 
+#define ROUNDUP(a) \
+		((a) > 0 ? (1 + (((a) - 1) | (sizeof(long) - 1))) : sizeof(long))
+#define ADVANCE(x, n) (x += ROUNDUP((n)->sa_len))
+
 static __dead void usage(void);
-static bool 	inaddreq(struct in_addr, struct in_addr);
 static bool 	httpget(CURL *, const char *);
-static struct sockaddr_in *find_sa(struct ifaddrs *, const char *);
 char	       *strsub(const char *, const char *, const char *);
+struct sockaddr *get_sa(char *, int);
 
 int
 main(int argc, char *argv[])
@@ -63,8 +71,6 @@ main(int argc, char *argv[])
 	if (NULL == yyin)
 		err(1, "fopen(\"%s\")", optf);
 
-	rt_socket();
-
 	struct ast *ast;
 	error = yyparse(&ast);
 	fclose(yyin);
@@ -78,50 +84,71 @@ main(int argc, char *argv[])
 	if (NULL == curl)
 		err(1, "curl_easy_init(3): failed to initialize libcurl");
 
+	/* set up route(4) socket */
+	int routefd = socket(PF_ROUTE, SOCK_RAW, AF_INET);
+	if (-1 == routefd)
+		err(1, "socket(2)");
+
+	unsigned int rtfilter = ROUTE_FILTER(RTM_NEWADDR);
+	if (-1 == setsockopt(routefd, PF_ROUTE, ROUTE_MSGFILTER,
+			     &rtfilter, sizeof(rtfilter)))
+		err(1, "setsockopt(2)");
+
 	if (!optd)
 		daemon(0, 0);
 
 	openlog(__progname, (optd ? LOG_PERROR : 0) | LOG_PID, LOG_DAEMON);
 	syslog(LOG_INFO, "starting dyndnsd-%s...", VERSION);
 
-	struct ifaddrs *ifap_old, *ifap_new;
-	getifaddrs(&ifap_old);
-
 	while (true) {
-		if (-1 == getifaddrs(&ifap_new)) {
-			syslog(LOG_ERR, "getifaddrs(3): %m");
-			goto sleep;
-		}
+		char buf[2048];
+
+		ssize_t numread = read(routefd, buf, sizeof(buf));
+		if (-1 == numread)
+			err(1, "read(2)");
+
+		struct rt_msghdr *rtm = (struct rt_msghdr *)buf;
+
+		assert(numread <= rtm->rtm_msglen);
+		assert(rtm->rtm_version == RTM_VERSION);
+		assert(rtm->rtm_type == RTM_NEWADDR);
+
+		struct ifa_msghdr *ifam = (struct ifa_msghdr *)rtm;
+		char ifname[IF_NAMESIZE];
+		if_indextoname(ifam->ifam_index, ifname);
+
+		struct sockaddr *sa = get_sa((char *)ifam + ifam->ifam_hdrlen, ifam->ifam_addrs);
+		struct sockaddr_in *sain = (struct sockaddr_in *)sa;
+
+		assert(sain->sin_family == AF_INET);
+
+		struct in_addr a;
+		memcpy(&a, &sain->sin_addr, sizeof(a));
+
+		assert(a.s_addr != INADDR_ANY);
+
+		char *ipaddr = inet_ntoa(a);
 
 		struct ast_iface *aif;
 		SLIST_FOREACH(aif, ast->interfaces, next) {
-			char *ipaddr;
-			struct sockaddr_in *sa_old, *sa_new;
-			sa_old = find_sa(ifap_old, aif->name);
-			sa_new = find_sa(ifap_new, aif->name);
-
-			if (!inaddreq(sa_old->sin_addr, sa_new->sin_addr))
+			if (0 != strcmp(aif->name, ifname))
 				continue;
 
-			ipaddr = inet_ntoa(sa_new->sin_addr);
+			struct ast_domain *ad;
+			SLIST_FOREACH(ad, aif->domains, next) {
+				char *url1 = strsub(aif->url, "$domain", ad->name);
+				char *url2 = strsub(url1, "$ip_address", ipaddr);
 
-			char *url1 = strsub(aif->url, "$domain", SLIST_FIRST(aif->domains)->name);
-			char *url2 = strsub(url1, "$ip_address", ipaddr);
+				if (httpget(curl, url2)) {
+					long status;
+					curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &status);
+					syslog(LOG_INFO, "%s %s %s %ld", aif->name, ipaddr, url2, status);
+				}
 
-			if (httpget(curl, url2)) {
-				long status;
-				curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &status);
-				syslog(LOG_INFO, "%s %s %s %ld", aif->name, ipaddr, url2, status);
+				free(url1);
+				free(url2);
 			}
-
-			free(url1);
-			free(url2);
 		}
-
-		freeifaddrs(ifap_old);
-		ifap_old = ifap_new;
-sleep:
-		sleep(60);
 	}
 
 	curl_easy_cleanup(curl);
@@ -135,12 +162,6 @@ usage(void)
 }
 
 static bool
-inaddreq(struct in_addr a, struct in_addr b)
-{
-	return memcmp(&a, &b, sizeof(struct in_addr)) == 0;
-}
-
-static bool
 httpget(CURL *curl, const char *url)
 {
 	curl_easy_setopt(curl, CURLOPT_URL, url);
@@ -150,17 +171,6 @@ httpget(CURL *curl, const char *url)
 		return false;
 	}
 	return true;
-}
-
-static struct sockaddr_in *
-find_sa(struct ifaddrs *ifa, const char *ifname)
-{
-	while (ifa) {
-		if (0 == strcmp(ifa->ifa_name, ifname))
-			return (struct sockaddr_in *)ifa->ifa_addr;
-		ifa = ifa->ifa_next;
-	}
-	return NULL;
 }
 
 char *
@@ -182,4 +192,21 @@ strsub(const char *src, const char *search, const char *replace)
 	strlcat(buf, src, sizeof(buf));
 
 	return strdup(buf);
+}
+
+struct sockaddr *
+get_sa(char *cp, int flags) {
+	int i;
+	struct sockaddr *sa;
+	if (0 == flags)
+		return NULL;
+	for (i = 1; i; i <<= 1) {
+		if (flags & i) {
+			sa = (struct sockaddr *)cp;
+			if (i == RTA_IFA)
+				return sa;
+			ADVANCE(cp, sa);
+		}
+	}
+	return NULL;
 }
