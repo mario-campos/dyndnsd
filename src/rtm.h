a5981ab824aa016c8691a63dd76ec57be7d9fc4e
Mario Campos <mario@mariocampos.io>
2017-07-11 20:48:24 -0500

    add comments to rtm.h

--- src/rtm.h
+++ src/rtm.h
@@ -3,8 +3,14 @@
 
 #include <net/route.h>
 
+/*
+ * Parse a Route Message for an interface name.
+ */
 char *rtm_getifname(struct rt_msghdr *);
 
+/*
+ * Parse a Route Message for an IP address.
+ */
 char *rtm_getipaddr(struct ifa_msghdr *);
 
 #endif /* RTM_H */
