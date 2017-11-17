361c49678ce2426c2fbdaefe3bd7c006b5500a64
Mario Campos <mario@mariocampos.io>
2017-11-16 21:48:56 -0600

    include macro for including file location
    for easier debugging

--- src/serr.h
+++ src/serr.h
@@ -1,6 +1,10 @@
 #ifndef SERR_H
 #define SERR_H
 
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#define AT(x) "[" __FILE__ ":" TOSTRING(__LINE__) "] " x
+
 __dead void serr(int, int, const char *);
 __dead void serrx(int, int, const char *);
 
