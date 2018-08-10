12a7e42c788dfb9830482d87d0a508e8afac3ba9
Mario Campos <mario@mariocampos.io>
2018-08-10 16:29:34 -0500

    use hex numbers instead of bit shifting. more conventional

--- src/dyndnsd.h
+++ src/dyndnsd.h
@@ -2,8 +2,8 @@
 #define DYNDNSD_H
 
 #define DYNDNSD_VERSION "1.0.0"
-#define DYNDNSD_DEBUG_MODE (1 << 1)
-#define DYNDNSD_VALID_MODE (1 << 2)
+#define DYNDNSD_DEBUG_MODE 0x0001
+#define DYNDNSD_VALID_MODE 0x0002
 
 extern FILE *yyin;
 extern int   yyparse();
